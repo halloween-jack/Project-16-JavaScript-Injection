0e4bf01cb0eb2fdde2ffd63c5c71929fc1d6b6d4
halloween-jack <halloween.jack.dev@gmail.com>
2017-10-21 01:21:16 +0900

    Adding an extension: NSExtensionItem

---
+++ Extension/ActionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by slim on 2017/10/21.
+//  Copyright © 2017 halloween-jack. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error) in
+                }
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
